f1fc15d5cb5b82be1b32939964844346e323f68a
David Gong <dgong2@jhu.edu>
2014-10-02 14:55:08 -0400

    Added setup function in mcast.c

--- mcast.c
+++ mcast.c
@@ -8,37 +8,92 @@ struct token_structure token_generate() {
   return t;
 };
 
+void setup(struct sockaddr_in *name, struct sockaddr_in *send_addr, int*
+            mcast_addr, struct ip_mreq *mreq, unsigned char *ttl_val, int *ss,
+            int *sr, fd_set *mask, fd_set *dummy_mask, fd_set *temp_mask) {
+  /* waits for the start_mcast message to start the actual process */
+    *mcast_addr = 225 << 24 | 0 << 16 | 1 << 8 | 1; // Will be changed later
+
+    *sr = socket(AF_INET, SOCK_DGRAM, 0); /* Socket for receiving */
+    if (*sr < 0) {
+        perror("Mcast: socket");
+        exit(1);
+    }
+
+    name->sin_family = AF_INET;
+    name->sin_addr.s_addr = INADDR_ANY;
+    name->sin_port = htons(PORT);
+
+    if (bind( *sr, (struct sockaddr *) name, sizeof(*name) ) < 0) {
+        perror("Mcast: bind");
+        exit(1);
+    }
+
+    mreq->imr_multiaddr.s_addr = htonl( *mcast_addr );
+
+    mreq->imr_nterface.s_addr = htonl( INADDR_ANY );
+
+    if (setsockopt(*sr, IPPROTO_IP, IP_ADD_MEMBERSHIP, (void *)mreq,
+        sizeof(*mreq)) < 0) {
+        perror("Mcast: problem in setsockopt to join multicast address" );
+    }
+
+    *ss - socket(AF_INT, SOCK_DGRAM, 0); /* Socket for sending */
+
+    if (ss < 0) {
+        perror("Mcast: socket");
+        exit(1);
+    }
+
+    *ttl_val = 1;
+    if (setsockopt(*ss, IPPROTO_IP, IP_MULTICAST_TTL, (void *)ttl_val,
+        sizeof(*ttl_val)) < 0 ) {
+        printf("Mcast: problem in setsockopt of multicast ttl %d - ignore in
+            WinNT or Win95\n", *ttl_val);
+    }
+
+    send_addr->sin_family = AF_INET;
+    send_addr->sin_addr.s_addr = htonl(mcast_addr);
+    send_addr->sin_port = htons(PORT);
+
+    FD_ZERO( mask );
+    FD_ZERO( &dummy_mask );
+    FD_SET( sr, mask );
+    FD_SET( (long)0, &mask );
+
+};
+
 void update_token(struct token_structure *t, int sequence) {
   /* updates the token with sequence number */
-}
+};
 
 int write_log(struct initializers *i) {
   /*writes to log for all received data */
-}
+};
 
 void update_rtr(struct initializers *i){
   /*Writes rtr to token based on missing packets */
-}
+};
 
 void send_data(struct initializers *i, struct token_structure *t){
   /*sends data up to fcc*/
-}
+};
 
 void send_token(struct token_structure *t){
   /*sends the current token to the next process*/
-}
+};
 
 void add_packet(struct initializers *i,struct packet_structure *p){
   /*adds an incoming packet to the data structure.*/
-}
+};
 
 void send_rtr_packets(struct initializers *i, struct token_structure *t){
   /* sends packets needed to be retransmitted from token rtr */
-}
+};
 
 void generate_packet(struct initializers *i){
   /* Generates the next packet, and */
-}
+};
 
 int parseargs(int argc, char **argv, struct initializers *i)
 {
@@ -60,6 +115,21 @@ int parseargs(int argc, char **argv, struct initializers *i)
 int main(int argc, char **argv)
 {
 	/* Variables */
+    struct sockaddr_in name;
+    struct sockaddr_in send_addr;
+
+    int                mcast_addr;
+
+    struct ip_mreq     mreq;
+    unsigned char      ttl_val;
+
+    int                ss,sr;
+    fd_set             mask;
+    fd_set             dummy_mask,temp_mask;
+
+    setup(&name, &send_addr, &mcast_addr, &mreq, &ttl_val, &ss, &sr, &mask,
+            &dummy_mask, &temp_mask);
+
 	struct initializers *i=malloc(sizeof(struct initializers));
 	struct token_structure *t=malloc(sizeof(struct token_structure));
 	struct packet_structure *p=malloc(sizeof(struct packet_structure));
